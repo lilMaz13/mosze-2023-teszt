0724dc6e3745c412562dcd33a4ee5fe5d3b48d95
lilmaz13 <petimazsi08@gmail.com>
2026-03-03 12:31:12 +0100

    Fixed the errors in the main.cpp file.

--- main.cpp
+++ main.cpp
@@ -5,36 +5,35 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     // HIBA: NELEMENTS nincs definialva. Valoszinuleg N_ELEMENTS akart lenni.
-    int *b = new int[NELEMENTS];
+    int* b = new int[N_ELEMENTS];
 
     // HIBA: szoveghez dupla idezojel kell, nem aposztrof (').
     // HIBA: hianyzik a pontosvesszo a sor vegerol.
-    std::cout << '1-100 ertekek duplazasa'
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
     
     // HIBA: for ciklus hibas (hianyzik a feltetel es a lepes/inkrement resz).
-    for (int i = 0;)
+    for (int i = 0; i < N_ELEMENTS; ++i)
     {
-        b[i] = i * 2;
+        b[i] = (i + 1) * 2;
     }
     // HIBA: feltetel "i" rossz. KELLENE: i < N_ELEMENTS
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; ++i)
     {
         // HIBA: hianyzik a kiirt ertek (pl. b[i]) es a pontosvesszo.
-        std::cout << "Ertek:"
-    }    
+        std::cout << "Ertek: " << b[i] << std::endl;
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
     
     // HIBA: atlag nincs inicializalva (szemet ertekrol indulna).
-    int atlag;
-    // HIBA: a for feltetel reszben vesszo van pontosvesszo helyett.
-    for (int i = 0; i < N_ELEMENTS, i++)
+    long long osszeg = 0;
+    for (int i = 0; i < N_ELEMENTS; ++i)
     {
-        // HIBA: hianyzik a pontosvesszo a sor vegerol.
-        atlag += b[i]
+        osszeg += b[i];
     }
-    atlag /= N_ELEMENTS;
+    double atlag = static_cast<double>(osszeg) / N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
 
     // HIBA: hianyzik a delete[] b; (memory leak).
+    delete[] b;
     return 0;
 }
